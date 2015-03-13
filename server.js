bec1a1c8950475c3326bdcfebaa4a7f8e42d812f
Kunal Nagar <knlnagar@gmail.com>
2015-03-14 01:18:43 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,36 @@
+var express = require('express');
+var multer = require('multer');
+
+var app = express();
+
+app.use(multer({
+	dest: './uploads/',
+	onFileUploadStart: function(file, req, res) {
+		res.write('starting file upload...\n');
+		res.write('\n\n');
+	},
+	onFileUploadData: function(file, data, req, res) {
+		var fileSize = req.get('Content-Length');
+		var doneSoFar = file.size;
+		var percentage = Math.ceil((doneSoFar/fileSize) * 100);
+		res.write('\n');
+		res.write(percentage + '% done');
+		res.write('\n');
+	},
+	onFileUploadComplete: function(file, req, res) {
+		res.write('\n\n\ncompleting file upload...\n');
+	}
+}))
+
+app.get('/', function(req, res) {
+	res.sendfile('./index.html');
+})
+
+app.post('/upload', function(req, res) {
+	res.write(JSON.stringify(req.files));
+	res.end('\n\n\nfile uploaded!\n');
+})
+
+app.listen(3000, function() {
+	console.log('listening on port 3000');
+})
