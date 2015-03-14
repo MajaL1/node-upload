670d0c7a8501a8c8cb426c63ee7a691eab0f20c7
Kunal Nagar <knlnagar@gmail.com>
2015-03-14 17:24:34 +0530

    using formidable instead of multer

--- server.js
+++ server.js
@@ -1,41 +1,33 @@
+var formidable = require('formidable');
+var util = require('util');
+
 var express = require('express');
-var multer = require('multer');
-
+var http = require('http');
 var app = express();
+var server = http.createServer(app);
 
-app.use(multer({
-	dest: './uploads/',
-	onFileUploadStart: function(file, req, res) {
-		res.write('starting file upload...\n');
-		res.write('\n\n');
-	},
-	onFileUploadData: function(file, data, req, res) {
-		var fileSize = req.get('Content-Length');
-		var doneSoFar = file.size;
-		var percentage = Math.ceil((doneSoFar/fileSize) * 100);
-		res.write('\n');
-		res.write(percentage + '% done');
-		res.write('\n');
-	},
-	onFileUploadComplete: function(file, req, res) {
-		res.write('\n\n\ncompleting file upload...\n');
-	}
-}))
+server.listen(3000, function() {
+	console.log('listening on port 3000');	
+});
 
 app.get('/', function(req, res) {
-	res.sendfile('./index.html');
+	res.sendFile(__dirname + '/index.html');
 })
 
 app.post('/', function(req, res) {
-	res.write(JSON.stringify(req.files));
-	res.end('\n\n\nfile uploaded!\n');
-})
-
-// app.post('/upload', function(req, res) {
-// 	res.write(JSON.stringify(req.files));
-// 	res.end('\n\n\nfile uploaded!\n');
-// })
-
-app.listen(process.env.PORT || 3000, function() {
-	console.log('listening on port 3000');
+	var form = new formidable.IncomingForm();
+	form.uploadDir = __dirname + '/uploads';
+	form.keepExtensions = true;
+	form.parse(req);
+	var percentages = [];
+	form.on('progress', function(bytesRec, bytesExp) {
+		var fileSize = bytesExp;
+		var doneSoFar = bytesRec;
+		var percentage = Math.ceil((doneSoFar/fileSize) * 100);
+		res.write(percentage.toString());
+		res.write('\n');
+	})
+	form.on('end', function() {
+		res.end('complete.');
+	})
 })
